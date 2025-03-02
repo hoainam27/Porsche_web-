7c27d3ca202f39371233ddd303a3d3260627e483
doohuy <domanhhuyy@gmail.com>
2025-03-02 13:38:15 +0700

    test

---
+++ CarSell/src/java/context/DBContext.java
@@ -0,0 +1,42 @@
+package context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FPT University - PRJ301
+ */
+public class DBContext {
+    public Connection connection;
+    public Connection getConnection()throws Exception {
+        String url = "jdbc:sqlserver://"+serverName+":"+portNumber + "\\" + instance +";databaseName="+dbName;
+        if(instance == null || instance.trim().isEmpty())
+            url = "jdbc:sqlserver://"+serverName+":"+portNumber +";databaseName="+dbName;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }   
+    /*Insert your other code right after this comment*/
+    /*Change/update information of your database connection, DO NOT change name of instance variables in this class*/
+    private final String serverName = "localhost";
+    private final String dbName = "PorscheDB";
+    private final String portNumber = "1433";
+    private final String instance="";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
+    private final String userID = "sa";
+    private final String password = "123456";
+    
+    public static void main(String[] args) {
+        try {
+            System.out.println(new DBContext().getConnection());
+        } catch (Exception e) {
+        }
+    }
+}
